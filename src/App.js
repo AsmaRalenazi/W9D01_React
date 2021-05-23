8ca2a04714c7354616c8676fd24cc81d9749c050
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-23 23:55:13 +0300

    solve

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+// jsx
+const App = () => {
+  const todos = [
+    {todo: "wake up", id: 1},
+    {todo: "breakfast", id: 2},
+    {todo: "study", id: 3}
+  ]
+  return<div><h1>hello world</h1></div>;
+};
+
+export default App
